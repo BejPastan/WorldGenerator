ad27d1322549fac8d847d99a1f8b58aa7468c1ac
BejPastan <s.synaszko@gmail.com>
2026-08-12 08:17:11 +0200

    Update PlateGeneration.py

--- WorldGenerator/WorldGenerator/Resources/Python/PlateGeneration.py
+++ WorldGenerator/WorldGenerator/Resources/Python/PlateGeneration.py
@@ -44,13 +44,9 @@ def generate_random_points_on_sphere(pointsNum:int, sphereSize:float) -> np.ndar
     """
     rng = np.random.default_rng()
     points = rng.integers(1, pointsNum*5, pointsNum) # generate random points in a linear space
-    print(points)
     points = np.array([(points/PHI)%1, points/((pointsNum*5) - 1)]) # calculate coordinates on plane
-    print(points)
     points = np.array([2*np.pi * points[0], np.acos(1-2*points[1])]) # transform to sphere(polar coordinates)
-    print(points)
     points = np.array([sphereSize * np.cos(points[0]) * np.sin(points[1]), sphereSize * np.sin(points[0]) * np.sin(points[1]), sphereSize * np.cos(points[1])]) # transform to cartesian coordinates
-    print(points)
     points = points.T # transpose to get the correct shape
     return points
 
@@ -70,23 +66,9 @@ def generate_voronoi(points:np.ndarray, sphereSize:float)-> spatial.SphericalVor
     """
     voronoi = spatial.SphericalVoronoi(points, radius=sphereSize, center=[0, 0, 0], threshold=1e-6)
     vertices = voronoi.vertices
-    #check_vertices = vertices
     vertices = vertices.T
     vertices = np.array([np.atan2(vertices[1], vertices[0])* 180/np.pi, np.arcsin(np.clip(vertices[2]/sphereSize, -1.0, 1.0))* 180/np.pi]) # transform to lat/lng
     vertices = vertices.T
-    # Check if there is any NAN value, if yet, print appropriate value from check vertices
-    # Create a mask that is True for any row containing at least one NaN
-    # nan_mask = np.isnan(vertices).any(axis=1)
-
-    # # Check if any NaN exists across the whole array
-    # if np.any(nan_mask):
-    #     print(f"Found {np.sum(nan_mask)} invalid vertices (NaN):")
-        
-    #     # Extract and print only the bad 3D vertices from check_vertices
-    #     bad_original_vertices = check_vertices[nan_mask]
-    #     print(bad_original_vertices)
-    #     raise(bad_original_vertices)
-
 
     region_counts = np.fromiter([len(region) for region in voronoi.regions], dtype=int)
     regions = np.concatenate(voronoi.regions)
@@ -109,30 +91,15 @@ def free_ndarray(toClear:np.ndarray):
     gc.collect()
 
 
-# platesNum = 15
-# segmentsNum = 10000
-# planetSize = 5000
-# points =generate_uniform_points_on_sphere(segmentsNum, planetSize, 0)
-# regionPoints = generate_random_points_on_sphere(platesNum, planetSize)
-# pointsToRegions = assign_points_to_regions(points, regionPoints)
-# voronoi = generate_voronoi(points, 10)[0]
-# print(len(voronoi))
-
-
-# # test loop
-# i=0
-# max_loop = 10000
-# platesNum = 15
-# segmentsNum = 10000
-# planetSize = 5000
-# badVertices = []
-# while i<max_loop:
-#     try:
-#         points =generate_uniform_points_on_sphere(segmentsNum, planetSize, 0)
-#         generate_voronoi(points, planetSize)
-#         i+=1
-#     except Exception as e:
-#         print("loop: ", i)
-#         badVertices.push(e)
-
-# print(badVertices)
+platesNum = 15
+segmentsNum = 10000
+planetSize = 5000
+i=0
+loop_num = 100
+while i<loop_num:
+    points =generate_uniform_points_on_sphere(segmentsNum, planetSize, 0)
+    regionPoints = generate_random_points_on_sphere(platesNum, planetSize)
+    pointsToRegions = assign_points_to_regions(points, regionPoints)
+    distinct_regions = np.unique(pointsToRegions)
+    print(len(distinct_regions))
+    i+=1
